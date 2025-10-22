64b2fe37a994d378cfa187a839c0c4f6d66bd54c
Aleksa <10934227@itisrossi.vi.it>
2025-10-22 08:14:11 +0200

    ciao

---
+++ src/APIQuestion.java
@@ -0,0 +1,9 @@
+public class APIQuestion {
+    public String question;
+    public String correct_answer;
+    public String category;
+    public String difficulty;
+    public String[] incorrect_answers;
+
+   // public Map<Character, String> answersList = new LinkedHashMap<>();
+}
